01c807d16e20d2fbfc8da02dab16cafa808bae63
VascoLucas <vascoalucas1245@gmail.com>
2024-08-12 09:54:13 +0100

    12-08-24

--- Visualize_markers.py
+++ Visualize_markers.py
@@ -13,61 +13,134 @@ small_names = {
 
 def start() -> None:
     # Import working directories
-    from globals import H5AD_DIR, CLUSTER_FUSION_DIR, Clustresults
-    from globals import DATASETS, lineage_resolution_tests, ENV
+    from globals import H5AD_DIR, Clustresults
+    from globals import DATASETS, ENV
     from helper_functions import random_colors
+    
+
+    # STEPS TODO
+        # 1. Load literature marker files.
     
     for d in DATASETS:
         
-        
-        # STEPS TODO
-        # 1. Load literature marker files.
         dest = f"{ENV}/{d}_Markers.csv"
         if os.path.exists(dest):
             print("Load cluster fusion file...")
             print(dest)
-            cluster_df = pd.read_csv(dest,
+            marker_df = pd.read_csv(dest,
                                      sep=';',
                                      header=0,
                                      dtype='category',
                                      index_col=None,
                                      na_filter=False)
-            print(cluster_df)
+            print(marker_df)
         else:
             continue
 
         # 2. Load entire data (with all genes). File 'raw_norm_annot.h5ad'
-        dest = f"{H5AD_DIR}/adata_final_{d}_raw_norm_annot.h5ad"
+        
+        dest = f"{H5AD_DIR}/adata_final_{d}_raw_norm_ranked.h5ad"
         if os.path.exists(dest):    # checks if the batch-corrected data already exists
             print("Load batch corrected data...")
             print(dest)
-            adata_raw = sc.read_h5ad(dest)
+            adata = sc.read_h5ad(dest)
             
-            print(adata_raw)
-                        
+            print(adata)
+        
         else:
             continue
+        
+        cell_type_biomarkers = marker_df.groupby('Cell')['Gene'].apply(list).to_dict()
 
-        # 3. Visualize DGE markers (sc.pl.(...dotplot rank...)) 
-        inj = adata_raw.obs["injury"]
+        print(cell_type_biomarkers)
+
+        
         
 
+
+        # 3. Visualize DGE markers ) 
+
+        adata_dge = adata.uns["rank_genes_groups_leiden_fusion"] 
+        gene_markers = marker_df["Gene"]
+        cell_types = marker_df["Cell"].unique()
+
+
+        fig, ax = plt.subplots(figsize=(25, 25))
+
+        sc.pl.rank_genes_groups_dotplot(adata= adata, 
+                                        groupby= None, 
+                                        values_to_plot= "logfoldchanges", 
+                                        min_logfoldchange=1, 
+                                        key="rank_genes_groups_leiden_fusion",
+                                        use_raw=None, 
+                                        show=False,
+                                        vmin=-3,
+                                        vmax=3,  
+                                        cmap='bwr',
+                                        ax=ax)
         
-        sc.pl.dotplot(adata = adata_raw, var_names= cluster_df, groupby = inj , use_raw=False, log=False, dendrogram=True)
+        ax.legend(loc='center left', bbox_to_anchor=(1.05, 0.5), frameon=False)
+        plt.title(f'{d} Clusters Differential Gene Expression')
+        fig.savefig(fname=f"{Clustresults}/{d}_denovo_dotplot.png", dpi=300, bbox_inches='tight')
+        plt.close(fig)
 
         # 4. Vizualize literature markers (sc.pl.dotplot) -> dar a lista de genes de marcadores e a lista de tipos de celulas
+        
+        fig, ax = plt.subplots(figsize=(7, 7))
+    
 
-        cell_types = {}
-        cell_types['M1 macrophage'] = ['CD80', 'CD86']
-        cell_types['M2 macrophage'] = ['MRC1', 'IL-10']
+        sc.pl.dotplot(adata = adata,
+                      var_names = cell_type_biomarkers, 
+                      groupby = "leiden_fusion", 
+                      use_raw=None, 
+                      log=False,
+                      standard_scale ='var', 
+                      ax=ax,
+                      )  
+        
+        ax.legend(loc='center left', bbox_to_anchor=(1.05, 0.5), frameon=False)
+        plt.title(f'{d} Cluster Cell Type and Gene Expression')
+        fig.savefig(fname=f"{Clustresults}/{d}_Cell_Type_Gene_expression_dotplot.png", dpi=300, bbox_inches='tight')
+        plt.close(fig)
+
+        #sc.pl.dotplot(adata = adata, var_names = marker_df, groupby = cell_types, use_raw=None, log=False, num_categories=0)
+
+        #for cell_type in cell_types:
+            #sc.pl.dotplot(adata, var_names=gene_markers[gene_markers.isin(marker_df.loc[marker_df['Cell'] == #cell_type, 'Gene'])], groupby='leiden_fusion', use_raw=None, log=False, num_categories=0)
+        
+
+
 
 
         # 5. Vizualize literature markers (pl.umap), do a umap for each gene
         
 
+        fig, ax = plt.subplots(figsize=(7, 7))
+        colors = random_colors(len(adata_dge.cat.categories.to_list()))
+
+        sc.pl.umap(adata,
+                   use_raw=False,          # gene expression
+                   color='leiden_fusion',
+                   wspace=0.3,
+                   #ncols=3,
+                   palette=colors,
+                   neighbors_key='neighbors_15',
+                   show=False,
+                   ax=ax)
+        ax.legend(loc='center left', bbox_to_anchor=(1.05, 0.5), frameon=False)
+        plt.title(f'{d} Differential Gene Expression')
+        fig.savefig(fname=f"{Clustresults}/{d}_umap_differential_gene_expression.png", dpi=300, bbox_inches='tight')
+        plt.close(fig)
+
+        #cell_types = {}
+        #cell_types['M1 macrophage'] = ['CD80', 'CD86']
+        #cell_types['M2 macrophage'] = ['MRC1', 'IL-10']
+
+       
+        
         
 
        
-       
 
-      
+
+start()
